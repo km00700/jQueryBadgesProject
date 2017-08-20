ee43ac6fb66fa653e573d19eee0be1616bbb85d1
km00700 <km00700@gmail.com>
2017-08-20 14:40:11 -0700

    Added ajax call and handler function

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,39 @@
 $(function() {
+	$.ajax({
+    	url: 'https://www.codeschool.com/users/km00700.json',
+    	dataType: 'jsonp',
+    	success: function(response) {
+      		console.log('response', response.courses.completed);
+      		addCourses(response.courses.completed);
+		}
+	});
 
-  // your code will go here
+	function addCourses(courses) {
+		
+		var $badges = $('#badges');
 
+		courses.forEach(function(course) {
+			
+			console.log(course);
+
+			var $course = $('<div />', {
+				'class': 'course'
+			}).appendTo($badges);
+
+			$('<h3 />', {
+				text: course.title
+			}).appendTo($course);
+
+			$('<img />', {
+				src: course.badge
+			}).appendTo($course);
+
+			$('<a />', {
+				'class': 'btn btn-primary',
+				target: '_blank',
+				href: course.url,
+				text: 'See Course'
+			}).appendTo($course);
+		})
+	}
 });
